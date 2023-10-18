d3add2b74d1de43157d300773a2d24d08768c370
SveenGmz <oswaldogomezu@gmail.com>
2023-10-18 01:45:22 -0600

    Fin del Backend

--- servidor.js
+++ servidor.js
@@ -21,5 +21,25 @@ http.createServer(function (request, response) {
         '.gif': 'image/gif',
         '.webp': 'image/webp',
         '.jpeg': 'image/jpeg'
-    }
-})
+    };
+
+    fs.readFile(filePath, function (error, content) {
+        if (error) {
+            if (error.code == 'ENOENT') {
+                fs.readFile('./404.html', function (error, content) {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                });
+            } else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: ' + error.code + '...\n');
+            }
+        } else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
+
+}).listen(3000);
+console.log('Server running at http://192.168.56.1:3000/');
+
